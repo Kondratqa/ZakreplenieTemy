32e04a6fddd3d45459a79aaa671dad720347a4eb
kinomagnum <kinomagnum@gmail.com>
2024-07-28 20:29:49 +0300

    Commit

--- src/Denis/Elka.java
+++ src/Denis/Elka.java
@@ -2,14 +2,18 @@ package Denis;
 
 public class Elka {
     public static void main(String[] args) {
-        int height = 10;
+        int height = 20; // Высота треугольника
+
         for (int i = 1; i <= height; i++) {
+            // Печатаем пробелы
             for (int j = height; j > i; j--) {
                 System.out.print(" ");
             }
+            // Печатаем звездочки
             for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("9");
+                System.out.print("*");
             }
+            // Переход на новую строку после каждой строки треугольника
             System.out.println();
         }
     }
