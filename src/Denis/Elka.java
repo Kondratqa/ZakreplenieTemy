ec9afbdf1b57d5e41045d21bc1b63e1d3a4e01ca
kinomagnum <kinomagnum@gmail.com>
2024-07-28 15:03:05 +0300

    ElkaCommit

---
+++ src/Denis/Elka.java
@@ -0,0 +1,16 @@
+package Denis;
+
+public class Elka {
+    public static void main(String[] args) {
+        int height = 10;
+        for (int i = 1; i <= height; i++) {
+            for (int j = height; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
