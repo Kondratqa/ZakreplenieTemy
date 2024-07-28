c3f39df4bb19534e4f6260f198ea659f39fe81f6
kinomagnum <kinomagnum@gmail.com>
2024-07-28 17:26:15 +0300

    initialKrest1000

--- src/Denis/Krest.java
+++ src/Denis/Krest.java
@@ -8,9 +8,9 @@ public class Krest {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (j == i || j == (width - 1 - i)) {
-                    System.out.print("0");
+                    System.out.print("1000");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
@@ -18,9 +18,9 @@ public class Krest {
         for (int i = 0; i < height + 4; i++) {
             for (int j = 0; j < width; j++) {
                 if (j == (height - 2 - i) || j == (width - (height - 1 - i))) {
-                    System.out.print("7");
+                    System.out.print("1000");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
