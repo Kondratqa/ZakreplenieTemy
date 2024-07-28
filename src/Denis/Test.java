7b34c36ce8b4c373db9b94ee2821831f038693a9
kinomagnum <kinomagnum@gmail.com>
2024-07-28 21:10:30 +0300

    testCommit

---
+++ src/Denis/Test.java
@@ -0,0 +1,14 @@
+package Denis;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 6;
+        int c = 7;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(x);
+        }for (int j = 0; j < 10; j++) {
+                System.out.print(c);
+            }
+        }
+    }
+
