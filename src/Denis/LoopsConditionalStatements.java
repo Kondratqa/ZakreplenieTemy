98b3c40401346f09267376be91f1330b51be716d
kinomagnum <kinomagnum@gmail.com>
2024-07-29 20:44:40 +0300

    dodelatCommit1

--- src/Denis/LoopsConditionalStatements.java
+++ src/Denis/LoopsConditionalStatements.java
@@ -2,10 +2,10 @@ package Denis;
 
 public class LoopsConditionalStatements {
     public static void main(String[] args) {
-        for (int y = 0; y <= 10; y++) { // количество строчек "координата Х"
+        for (int y = 0; y < 10; y++) { // количество строчек "координата Х"
             for (int x = 0; x < 10; x++) { // количество звездочек "координата y"
-                if (y >= 2 && y <= 7 && x >=2 && x <= 7) {
-                    System.out.print(" "); // Пробелы
+                if (y > 2 && y < 7) {
+                    System.out.print("0"); // Пробелы
                 } else {
                     System.out.print("*");
                 }
